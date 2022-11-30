217962777e8ebac38307585dc13b1e08acb7347e
Gabula John Stephen <77186258+Gabula145@users.noreply.github.com>
2022-11-30 19:02:52 +0300

    Update columns.dart

---
+++ lib/screens/columns.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Columns extends StatefulWidget {
+  const Columns({Key? key}) : super(key: key);
+
+  @override
+  State<Columns> createState() => _ColumnsState();
+}
+
+class _ColumnsState extends State<Columns> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
